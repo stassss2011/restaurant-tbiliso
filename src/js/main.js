1364352763c8c228d3b50a16224e37a05717a997
eir73 <roflaneir73@gmail.com>
2020-09-30 13:37:16 +0300

    add expanded bo=urger-menu

---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+const headerToggler = document.getElementById('navbar__toggler')
+const togglerIcon = headerToggler.querySelector('.material-icons')
+const navbarMenu = document.getElementById('navbar__menu')
+
+const scrollbarWidth = window.innerWidth - document.body.clientWidth;
+
+console.log(scrollbarWidth)
+
+headerToggler.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open')
+    if(navbarMenu.classList.contains('open')) {
+        togglerIcon.innerHTML = 'close'
+        document.body.style.position = 'fixed'
+        document.body.style.paddingRight = scrollbarWidth + 'px'
+    } else {
+        togglerIcon.innerHTML = 'menu'
+        document.body.style.position = 'unset'
+        document.body.style.paddingRight = '0'
+    }
+})
