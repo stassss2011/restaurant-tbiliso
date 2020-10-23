4f652059fc2266289c1f8e37bdd1b645d30856e0
eir73 <roflaneir73@gmail.com>
2020-10-23 21:38:08 +0300

    qa

--- js/main.js
+++ js/main.js
@@ -5,65 +5,99 @@ const navbarMenu = document.getElementById('navbar__menu')
 const scrollbarWidth = window.innerWidth - document.body.clientWidth;
 
 headerToggler.addEventListener('click', () => {
-    navbarMenu.classList.toggle('open')
-    if(navbarMenu.classList.contains('open')) {
-        togglerIcon.innerHTML = 'close'
-        document.body.style.position = 'fixed'
-        document.body.style.paddingRight = scrollbarWidth + 'px'
-    } else {
-        togglerIcon.innerHTML = 'menu'
-        document.body.style.position = 'unset'
-        document.body.style.paddingRight = '0'
-    }
+  navbarMenu.classList.toggle('open')
+  if (navbarMenu.classList.contains('open')) {
+    togglerIcon.innerHTML = 'close'
+    document.body.style.position = 'fixed'
+    document.body.style.paddingRight = scrollbarWidth + 'px'
+  } else {
+    togglerIcon.innerHTML = 'menu'
+    document.body.style.position = 'unset'
+    document.body.style.paddingRight = '0'
+  }
 })
 
-if(window.innerWidth <= 992) {    
-    const swiper = new Swiper('.hall__items', {
-        slideClass: 'hall__item',
-        slidesPerView: 1,
-        pagination: {
-            el: '.swiper-pagination'
-        }
-    })
-
-    const swiperEvents = new Swiper('.event__items', {
-        slideClass: 'event__item',
-        slidesPerView: 1,
-        pagination: {
-            el: '.swiper-pagination'
-        }
-    })
-}
-
-const bookForm = document.getElementById('book-form') 
-const delivForm = document.getElementById('delivery-form')
-
-if (bookForm) {
-
-    bookForm.onsubmit = e => {
-        e.preventDefault()
-        const name = document.getElementById('name').value
-        const phone = document.getElementById('phone').value
-        const date = document.getElementById('date').value
-        
-        const data = {name, phone, date}
-
-        console.log(data)
+if (window.innerWidth <= 992) {
+  const swiper = new Swiper('.hall__items', {
+    slideClass: 'hall__item',
+    slidesPerView: 1,
+    pagination: {
+      el: '.swiper-pagination'
     }
-}
+  })
 
-if (delivForm) {
-    delivForm.onsubmit = e => {
-        e.preventDefault()
-        const name = document.getElementById('name').value
-        const phone = document.getElementById('phone').value
-        const date = document.getElementById('date').value
-        const address = document.getElementById('address').value
-
-        const data = {name, phone, date, address}
-
-        console.log(data)
-
+  const swiperEvents = new Swiper('.event__items', {
+    slideClass: 'event__item',
+    slidesPerView: 1,
+    pagination: {
+      el: '.swiper-pagination'
     }
+  })
 }
+
+const bookForm = document.getElementById('book-form')
+const deliveryForm = document.getElementById('delivery-form')
+
+bookForm.onsubmit = e => {
+  e.preventDefault()
+  const name = bookForm.querySelector('#name').value
+  const phone = bookForm.querySelector('#b-phone').value
+  const date = bookForm.querySelector('#book-date').value
+  const data = { name, phone, date }
+  console.log(data)
+}
+
+deliveryForm.onsubmit = e => {
+  e.preventDefault()
+  const name = deliveryForm.querySelector('#name').value
+  const phone = deliveryForm.querySelector('#d-phone').value
+  const date = deliveryForm.querySelector('#delivery-date').value
+  const address = deliveryForm.querySelector('#address').value
+  const data = { name, phone, date, address }
+  console.log(data)
+}
+  
+// pop-up
+const bookLinks = [...document.querySelectorAll('a[href="./book.html"]')]
+const deliveryLinks = [...document.querySelectorAll('a[href="./delivery.html"]')]
+const bookBlock = document.querySelector('.book__form-wr')
+const bookWrap = document.querySelector('.book')
+const bookClose = document.getElementById('book-close')
+const deliveryBlock = document.querySelector('.delivery__form-wr')
+const deliveryWrap = document.querySelector('.delivery')
+const deliveryClose = document.getElementById('delivery-close')
+
+bookWrap.addEventListener('click', (e) => {
+  if (!bookBlock.contains(e.target) || e.target === bookClose) {
+    bookWrap.classList.remove('open')
+    bookWrap.classList.remove('no-transition')
+  }
+})
+
+bookLinks.forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault()
+    bookWrap.classList.add('open')
+    setTimeout(() => {
+      bookWrap.classList.add('no-transition')
+    }, 1000)
+  })
+})
+
+deliveryWrap.addEventListener('click', (e) => {
+  if (!deliveryBlock.contains(e.target) || e.target == deliveryClose) {
+    deliveryWrap.classList.remove('no-transition')
+    deliveryWrap.classList.remove('open')
+  }
+})
+
+deliveryLinks.forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault()
+    deliveryWrap.classList.add('open')
+    setTimeout(() => {
+      deliveryWrap.classList.add('no-transition')
+    }, 1000)
+  })
+})
 //# sourceMappingURL=main.js.map
